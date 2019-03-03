4cd356ff4efb55d37feee7d7b6653646c5008606
jllujan <40473883+jllujan@users.noreply.github.com>
2019-03-02 20:08:24 -0700

    Update AStarSearch.java
    Manhattan distance

--- AStarSearch.java
+++ AStarSearch.java
@@ -23,6 +23,16 @@ public class AStarSearch {
             float v = thisValue - otherValue;
             return (v > 0) ? 1 : (v < 0) ? -1 : 0; // sign function
         }
+        public int manhattanDistance(int[] start, int[] goal){
+            int distance = Math.abs(goal[0] - start[0]) + Math.abs(goal[1] - start[1]);
+            return distance;
+        }
+        public int pickHeuristic(String heuristic, int[] start, int[] goal) {
+            if(heuristic.equalsIgnoreCase("Manhattan"))
+                return manhattanDistance(start,goal);
+            else
+                return 0;
+        }
 
 
         public abstract float getCost(AStarNode node);
